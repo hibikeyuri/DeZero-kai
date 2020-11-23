87066175881989f0a57eabfb369a21c7b97ccd4f
Hibikeyuri <hibikeyuri@gmail.com>
2020-11-23 22:26:36 +0800

    god damn it

--- dezero/core.py
+++ dezero/core.py
@@ -350,3 +350,5 @@ def setup_variable():
 
 #You got a dream, you gotta protect it. People can't do something themselves, they wanna
 #tell you can't do it. If you want something, go get it. Period.
+
+# I'M FUCKING TIRED
